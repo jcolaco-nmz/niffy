eff52110eb494ae7b6bdc98b4032245fa6f97dde
João Colaço <joao.colaco@elgin.org.uk>
2016-10-06 14:43:01 +0100

    First iOS app version. Includes Notification handling.

---
+++ iOSApp/NiffyApp/NiffyApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NiffyApp
+//
+//  Created by Joao on 06/10/16.
+//  Copyright © 2016 João Colaço. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
