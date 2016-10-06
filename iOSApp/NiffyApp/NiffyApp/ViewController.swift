40ec4dbaffa2a40b687dec06f1f357b83165d760
João Colaço <joao.colaco@elgin.org.uk>
2016-10-06 15:43:41 +0100

    Notification receival.

--- iOSApp/NiffyApp/NiffyApp/ViewController.swift
+++ iOSApp/NiffyApp/NiffyApp/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib.
+        let url = URL(string: "https://apps.roadworks.org");
+        let requestObj = URLRequest(url: url!);
+        webView.loadRequest(requestObj);
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
